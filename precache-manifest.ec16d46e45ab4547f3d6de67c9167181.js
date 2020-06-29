aceeada8c418a0e1ee52d250fdd321e73c8b9a82
Hazem Krimi <krimihazem1@gmail.com>
2020-06-29 16:56:45 +0100

    Updates

---
+++ precache-manifest.ec16d46e45ab4547f3d6de67c9167181.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585e46f8f18271b62b8d4c04a7ff9b3c",
+    "url": "/pexta/index.html"
+  },
+  {
+    "revision": "4d1aba9f7ee3afcf0d03",
+    "url": "/pexta/static/css/main.46d00040.chunk.css"
+  },
+  {
+    "revision": "6a5360c031a3c0e2623c",
+    "url": "/pexta/static/js/2.7e2131bb.chunk.js"
+  },
+  {
+    "revision": "4d1aba9f7ee3afcf0d03",
+    "url": "/pexta/static/js/main.ea36e504.chunk.js"
+  },
+  {
+    "revision": "e3a4d301ab76b912fed2",
+    "url": "/pexta/static/js/runtime~main.3cad234d.js"
+  },
+  {
+    "revision": "3e6842eeae69e3e8e26fc3e0cb516dc1",
+    "url": "/pexta/static/media/home-page-wave.3e6842ee.svg"
+  },
+  {
+    "revision": "acee564143c93d791aa87a63d374688d",
+    "url": "/pexta/static/media/landing-page-wave.acee5641.svg"
+  },
+  {
+    "revision": "60cc297a6550e2db5fe7e2911993d2e8",
+    "url": "/pexta/static/media/post-page-wave.60cc297a.svg"
+  }
+]);
